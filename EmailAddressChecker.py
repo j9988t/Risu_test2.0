f7fc171e16333d46080bce36c753cc5ed94027d0
oeg8168 <oeg8168@gmail.com>
2017-01-09 15:45:52 +0800

    make good use of source code
    separate Risu_test2.0.py and EmailAddressChecker.py from README.md

---
+++ EmailAddressChecker.py
@@ -0,0 +1,47 @@
+import re
+
+
+class EmailAddressChecker():
+    def __init__(self):
+        self.emailName = self.getUserInput()      
+        self.printUserInput()
+        self.checkEmailAddress()
+        
+    def getUserInput(self):
+        userInput = raw_input('Key in an email address: ')
+        return userInput
+        
+    def printUserInput(self):
+        print('Your email address: ' + self.emailName)
+        
+    def checkEmailAddress(self):
+        if self.checkEmailMouse() == False:
+            self.printEmailAddressResult(False)
+            return
+        emailFrag = self.splitEmailAddress()
+        checkResult = (self.checkEmailLocalPart(emailFrag[0]) 
+                    & self.checkEmailDomain(emailFrag[1]))
+        self.printEmailAddressResult(checkResult)
+                
+    def checkEmailMouse(self):
+        numOfMouse = len(re.findall('@', self.emailName))
+        return(True if numOfMouse == 1 else False)
+        
+    def checkEmailLocalPart(self, emailLocalPart):
+        ###
+        
+        return True
+    
+    def checkEmailDomain(self, emailDomain):
+        ###
+        return False
+    
+    
+    def splitEmailAddress(self):
+        emailFrag = []
+        emailFrag = self.emailName.split('@')
+        return emailFrag
+    
+    def printEmailAddressResult(self, checkResult):
+        print('Your email address is ' + 
+              ('ACCEPTED !' if checkResult == True else 'REJECTED !'))
